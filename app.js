8e04b508d285b19ea39263689e2c82198f48519b
1712244 <1712244@student.hcmus.edu.vn>
2020-05-18 23:29:24 +0700

    trang log in (tinh su dung css) may cai function js de cho vui thoi :v

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+
 const livereload = require("livereload");
 
 var liveReloadServer = livereload.createServer();
@@ -17,9 +19,9 @@ var app = express();
 
 app.use(connectLivereload());
 liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 50);
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 50);
 });
 
 // view engine setup
@@ -34,21 +36,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/login', loginRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
