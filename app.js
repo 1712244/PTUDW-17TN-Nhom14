581f78ebdb44058038f282c0d5a3220b0c38133b
1712760 <phamtthang333@gmail.com>
2020-05-19 18:13:02 +0700

    Tạo chỗ khác nhau để merge

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+
+
 var signUpRouter = require('./routes/sign-up');
 
 var forgetpassRouter = require('./routes/forget-pass')
