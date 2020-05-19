19c55c8871493c0871b6811d20cee57408a66c33
1712244 <1712244@student.hcmus.edu.vn>
2020-05-19 10:50:04 +0700

    lam lai sign-up

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
+var signUpRouter = require('./routes/sign-up');
 
 const livereload = require("livereload");
 
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/login', loginRouter);
+app.use('/sign-up', signUpRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
--- routes/sign-up.js
+++ routes/sign-up.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('login');
+    res.render('sign-up');
 });
 
 module.exports = router;
