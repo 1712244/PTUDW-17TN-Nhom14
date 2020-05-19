618151bdbdedb0556bfae5d524f7d753e49c7bd5
1712760 <phamtthang333@gmail.com>
2020-05-19 18:18:18 +0700

    Merge pull request #4 from 1712244/trang_log_in
    trang log in sử dụng theo css, bỏ phần js đổi class

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+
 
 
 
@@ -41,6 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 app.use('/sign-up', signUpRouter);
 
