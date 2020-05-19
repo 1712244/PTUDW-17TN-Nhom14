6cba000fe815dc8226d2c8d36a241228cff89e49
1712760 <phamtthang333@gmail.com>
2020-05-19 20:14:15 +0700

    Hoàn thành cơ bản cho trang tin tức
    Thêm 2 file giao diện news.js/hbs và ảnh thông báo sinh viên trả sách trễ. 
    Thêm route cho app.js

---
+++ routes/news.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+
+
+const sectionSuKien = {
+  title: "Sự kiện",
+  other_news: "Các tin khác",
+  more_news: "→ xem tất cả sự kiện",
+  articles: [
+    {
+      title:"Hội sách khoa",
+      date:"01/01/2020",
+      thumbnail:"/images/news-thumbnail.png"
+    },
+    {
+      title:"Ngày hội đọc sách dành cho mọi người",
+      date:"01/01/2020",
+      thumbnail:"/images/news-thumbnail.png"
+    }
+  ]
+}
+
+const sectionThongBao = {
+    title: "Thông báo",
+    other_news: "Các tin khác",
+    more_news: "→ xem tất cả thông báo",
+    articles: [
+      {
+        title:"Danh sách sinh viên trả sách muộn",
+        date:"01/01/2020",
+        thumbnail:"/images/alert-late-thumbnail.png"
+      },
+      {
+        title:"Thông báo đăng ký mua sách",
+        date:"01/01/2020",
+        thumbnail:"/images/news-thumbnail.png"
+      }
+    ]
+  }
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('news', { title: 'Trang Tin Tức', sectionSuKien,sectionThongBao  });
+});
+
+
+
+
+module.exports = router;
