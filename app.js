634f9f2b81114e947da929e0b8af7d11b79715c1
1712760 <phamtthang333@gmail.com>
2020-05-19 18:15:44 +0700

    Tạo thêm merge conflict

--- app.js
+++ app.js
@@ -38,10 +38,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/sign-up', signUpRouter);
+
 // catch 404 and forward to error handler
 app.use('/forget-pass', forgetpassRouter)
     // catch 404 and forward to error handler
