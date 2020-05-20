fb9c44c56e86e9e47f8232406c24e0b41c360661
1712244 <1712244@student.hcmus.edu.vn>
2020-05-20 12:48:11 +0700

    hoan thanh trang chi tiet tin tuc

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var signUpRouter = require('./routes/sign-up');
+var newsDetailRouter = require('./routes/news-detail');
 
 const livereload = require("livereload");
 
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/sign-up', signUpRouter);
+app.use('/news-detail', newsDetailRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
