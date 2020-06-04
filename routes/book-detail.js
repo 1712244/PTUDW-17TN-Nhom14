26200bd1ebb55d8db1f72255b6d60335770ae8db
1712244 <1712244@student.hcmus.edu.vn>
2020-06-04 10:21:55 +0700

    chua co phan js xu ly, viet thang vao <script> vi ko import vo dc file rieng

--- routes/book-detail.js
+++ routes/book-detail.js
@@ -25,8 +25,7 @@ const sectionBook = {
     average_rate: 3.5,
     book_para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et sem nibh. Suspendisse ornare accumsan metus eget maximus. Sed suscipit molestie pellentesque. Morbi varius fermentum urna, bibendum dapibus odio fermentum at. Mauris rutrum justo dui, auctor sagittis enim suscipit ut. Vestibulum gravida dui quam, sit amet porta tortor malesuada id. Praesent ut pharetra sapien. Praesent sit amet consectetur lorem. Nam at mi non sapien efficitur porttitor vel quis nulla. Praesent pulvinar, ligula non molestie cursus, purus turpis tincidunt sem, eu sollicitudin ligula libero eget est. Cras dignissim tortor ante, et tincidunt odio dictum eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eleifend ante ac neque aliquet, at gravida lectus ultrices. Quisque id nulla neque. Sed quis mi at purus dictum volutpat. Aenean a malesuada risus, ut pulvinar nisl. Donec facilisis pellentesque ex, ut blandit ligula accumsan nec. Aliquam nibh nulla, elementum vitae mattis quis, consectetur et risus. Nullam quis turpis diam. Vestibulum iaculis eros vitae nunc volutpat egestas. Fusce fermentum at leo a ultrices. Nunc sollicitudin, risus non ultrices ultricies, magna urna pharetra lacus, sed vehicula leo quam id risus. Nam elementum sit amet nunc aliquet aliquam",
     book_image: "images/book-thumbnail.png",
-    user_comments: [
-        {
+    user_comments: [{
             avatar: '/images/news-thumbnail.png',
             username: 1712760,
             user_rate: 3,
@@ -39,7 +38,7 @@ const sectionBook = {
             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
         },
     ],
-    related_booklist: _related_booklist,
+    related_booklist: _related_booklist
 }
 
 function to_01(rate) {
@@ -61,9 +60,9 @@ sectionBook.user_comments.forEach(comment => {
     comment.user_rate = to_01(comment.user_rate);
 });
 
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
 
-    res.render('book-detail', {layout:"layout", sectionBook });
+    res.render('book-detail', { layout: "layout", sectionBook });
 });
 
 module.exports = router;
