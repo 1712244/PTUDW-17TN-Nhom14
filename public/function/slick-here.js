46eac182692c85fd73ddd1dcb505dcd7cb599b60
1712760 <phamtthang333@gmail.com>
2020-08-08 10:31:47 +0700

    Xong slickjs
    Carousel responsive nhìn mượt khi dùng ở lap. Đã sửa carousel ở trang chủ, trang kho sách và trang chi tiết sách

--- public/function/slick-here.js
+++ public/function/slick-here.js
@@ -23,3 +23,29 @@ $('.slick-book-here').slick({
       }]
     
   });
+
+  $('.slick-nav-book-here').slick({
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        }
+  
+      }, {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        }
+      },  {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }]
+    
+  });
