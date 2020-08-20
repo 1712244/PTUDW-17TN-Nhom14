3ef6ac2e26ad9d390e59ccc894be88643ecd6d4b
1712760 <phamtthang333@gmail.com>
2020-08-20 20:29:02 +0700

    xong parse storage
    Có sort theo thứ tự luôn rồi

--- routes/front-end/storage.js
+++ routes/front-end/storage.js
@@ -1,54 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
-const sectionSachHot =
-{
-  title: "Sách nhiều người đọc nhất",
-  books: [
-    [
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng", rates: 9999 },
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng", rates: 9999 },
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng", rates: 9999 },
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng", rates: 9999 },
-    ],
-    [
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng", rates: 9999 },
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng", rates: 9999 },
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng", rates: 9999 },
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng", rates: 9999 },
-    ],
-  ]
-};
-const sectionSach =
-{
-  title: "Sách chuyên ngành",
-  list_categories: [
-    ["Công nghệ phần mềm", "Trí tuệ nhân tạo", "Cơ sở dữ liệu",
-      "Mạng máy tính", "Khoa học máy tính", "Phát triển ứng dụng web"],
-    ["Công nghệ phần mềm", "Trí tuệ nhân tạo", "Cơ sở dữ liệu",
-      "Mạng máy tính", "Khoa học máy tính", "Phát triển ứng dụng web"],
-  ],
-  books: [
-    [
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng" },
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng" },
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng" },
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng" },
-    ],
-    [
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng" },
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng" },
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng" },
-      { thumbnail: "/images/book-thumbnail.png", name: "Kỹ nghệ phần mềm", author: "TS. Lê Văn Phùng" },
-    ],
-  ]
-};
+var storageController = require('./../../controllers/storage')
 
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('storage', {layout:"layout", title: 'Kho sách', sectionSachHot, sectionSach });
-});
+router.get('/', storageController.getStorage);
 
 
 
