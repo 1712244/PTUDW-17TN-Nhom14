3d90bd7521884482dfb2b801909aaf02ded8ebcc
1712760 <phamtthang333@gmail.com>
2020-08-20 21:54:24 +0700

    Hoàn thành check login khi vào detail book
    Detail book sẽ không cho tương tác 1 số thứ khi xem thông tin sách như không được nhấn nút mượn và không cho bình luận. 
    Hotfix: jquery không cho post do version slim.  Sửa lại jquery ở layout thành bản full là được.

--- public/function/login.js
+++ public/function/login.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
     const formLogin = $("#formLogin");
     const formSignup = $("#formSignup");
-  
     formLogin.submit(function () {
       body = $(this).serializeArray();
       $.post('/api/account/sign-in', body)
-        .done(function (data) {
+        .done(function (data) { 
           window.location.href = "/";
         })
         .fail(function (data) {
