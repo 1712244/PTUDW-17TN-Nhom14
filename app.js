9deb7d3bcaa5c9b605fa91468d7eaa4f0c0b3d85
1712760 <phamtthang333@gmail.com>
2020-08-21 21:03:44 +0700

    Xóa phần dùng để debug app.js

--- app.js
+++ app.js
@@ -58,11 +58,11 @@ app.use(
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(async function (req, res, next) {
-    // if (req.session.username){
-    //     res.locals.user = await accountService.getUserInfo(req.session.username);
-    // }
-    req.session.username = '1712760'
-    res.locals.user = { id: '1712760', username: '1712760' }
+    if (req.session.username){
+        res.locals.user = await accountService.getUserInfo(req.session.username);
+    }
+    // req.session.username = '1712760'
+    // res.locals.user = { id: '1712760', username: '1712760' }
     
 
     next();
