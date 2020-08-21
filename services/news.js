7755f9cda4f07ceef4350d788a26eb562ea3b20b
1712244 <1712244@student.hcmus.edu.vn>
2020-08-21 12:19:25 +0700

    parse new-detail

--- services/news.js
+++ services/news.js
@@ -1,15 +1,15 @@
 const News = require("./../collections/news");
 const dateTimeService = require('./../utils/dateTime');
 
-function createModel(title, content, tag, writer, type) {
+function createModel(title, desc, tag, writer, type) {
     const newNewsModel = new News({
         title: title,
-        content: content,
+        desc: desc,
         tag: tag,
         type: type,
         writer: writer,
         cDate: dateTimeService.now(),
-        mDate: dateTimeService.new()
+        mDate: dateTimeService.now()
     });
     return newNewsModel;
 }
@@ -25,7 +25,7 @@ function insert(newNews) {
 
 function getById(_id) {
     return new Promise((resolve, reject) => {
-        News.findOne({ _id: _id }).select("_id title content tag writer type cDate mDate").exec((error, newsDocument) => {
+        News.findOne({ _id: _id }).select("_id title desc date image_url type").exec((error, newsDocument) => {
             if (error) return reject(error);
             return resolve(newsDocument);
         });
@@ -34,7 +34,7 @@ function getById(_id) {
 
 function getAll() {
     return new Promise((resolve, reject) => {
-        News.find().select("_id title content tag writer type cDate mDate").exec((error, newsDocument) => {
+        News.find().exec((error, newsDocument) => {
             if (error) return reject(error);
             return resolve(newsDocument);
         });
@@ -51,9 +51,9 @@ function removeById(_id) {
 }
 
 
-function updateById(_id, title, content, tag, writer) {
+function updateById(_id, title, desc, tag, writer) {
     return new Promise((resolve, reject) => {
-        News.updateOne({ _id: _id }, { title: title, content: content, tag: tag, writer: writer, type: type }).exec((error) => {
+        News.updateOne({ _id: _id }, { title: title, desc: desc, tag: tag, writer: writer, type: type }).exec((error) => {
             if (error) return reject(error);
             return resolve(true);
         })
