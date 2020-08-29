beac4bfa7f881dedc491738fc3c8a038f6323faa
1712760 <phamtthang333@gmail.com>
2020-08-29 17:03:05 +0700

    hoàn thành profile / info user

--- routes/front-end/profile.js
+++ routes/front-end/profile.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-const profile = {
-    username: "ptthang",
-    name: "Phạm Trọng Thắng",
-    mssv: "1712760",
-    usercode: "1712760",
-    avatar:null,
-    qr_code:"/images/qr-code.svg"
-}
+
 
 
 router.get('/', function (req, res, next) {
+  const profile = {
+    username: res.locals.user.username,
+    name: res.locals.user.user.name,
+    mssv: res.locals.user.username,
+    usercode: res.locals.user.id,
+    avatar:res.locals.user.user.avatar,
+    qr_code:"/images/qr-code.svg"
+}
   res.render('profile', {layout: "layout", profile:profile  });
 });
 
